9cff82ed92ffec3b00152e610327575c10248db6
marious93 <marious_93@mail.ru>
2021-09-10 02:34:34 +1000

    Initial commit

---
+++ app/src/main/java/com/example/vksearch/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.vksearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static com.example.vksearch.NetUtil.generateURL;
+import static com.example.vksearch.NetUtil.getRespondfromURL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText searchField;
+    private Button searchButton;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            searchField = findViewById(R.id.search_field);
+        searchButton = findViewById(R.id.button);
+        result = findViewById(R.id.result);
+        searchButton.setOnClickListener(this::onClick);
+    }
+    class VKQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getRespondfromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+        protected void onPostExecute(String response){
+            String firstName = null;
+            String lastName =null;
+
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("response");
+                JSONObject userInfo= jsonArray.getJSONObject(0);
+                firstName = userInfo.getString("first name");
+                lastName = userInfo.getString("last name");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            String resultingString = "Имя: " + firstName + "\n" + "Фамилия" + lastName;
+            result.setText(resultingString);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        URL generateURL = generateURL( searchField.getText().toString());
+        new VKQueryTask().execute(generateURL);
+
+    }
+    }
---
+++ app/src/main/java/com/example/vksearch/NetUtil.java
@@ -0,0 +1,49 @@
+package com.example.vksearch;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetUtil {
+    private static final String vk_api = "https://api.vk.com/";
+    private static final String vk_user_get = "method/users.get";
+    private static final String user_id = "user_ids";
+    private static final String version = "v";
+    private static final String token = "access_token";
+    //&access_token=10259175b5475861292b276d0b2103f8a4fde6677edb8fee1002911529f0fcba84ad2cd1939662ba758a5&
+
+    public static URL generateURL(String userId) {
+        Uri buildUri = Uri.parse(vk_api + vk_user_get).buildUpon().appendQueryParameter(user_id, userId).
+                appendQueryParameter(version, "5.8").appendQueryParameter(token, "10259175b5475861292b276d0b2103f8a4fde6677edb8fee1002911529f0fcba84ad2cd1939662ba758a5").build();
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getRespondfromURL(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+
+        }
+    }
+}
