2ad95822109381a4a2f67e85a0dfb9ed8900b3cb
marious93 <marious_93@mail.ru>
2021-09-24 18:00:02 +1000

    Working version. Give info about ID user in vkontakte.

--- app/src/main/java/com/example/vksearch/MainActivity.java
+++ app/src/main/java/com/example/vksearch/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -17,13 +18,25 @@ import java.io.IOException;
 import java.net.URL;
 
 import static com.example.vksearch.NetUtil.generateURL;
-import static com.example.vksearch.NetUtil.getRespondfromURL;
+import static com.example.vksearch.NetUtil.getRespondFromURL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+    private TextView error;
+    private ProgressBar loading;
+
+    private void showResultText(){
+        result.setVisibility(View.VISIBLE);
+        error.setVisibility(View.INVISIBLE);
+    }
+
+    private void showErrorText(){
+        error.setVisibility(View.VISIBLE);
+        result.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +45,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             searchField = findViewById(R.id.search_field);
         searchButton = findViewById(R.id.button);
         result = findViewById(R.id.result);
+        error = findViewById(R.id.error);
+        loading = findViewById(R.id.loading_indicator);
         searchButton.setOnClickListener(this::onClick);
     }
     class VKQueryTask extends AsyncTask<URL, Void, String> {
 
         @Override
+        protected void onPreExecute() {
+            loading.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected String doInBackground(URL... urls) {
             String response = null;
             try {
-                response = getRespondfromURL(urls[0]);
+                response = getRespondFromURL(urls[0]);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -50,18 +70,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String firstName = null;
             String lastName =null;
 
-            try {
-                JSONObject jsonResponse = new JSONObject(response);
-                JSONArray jsonArray = jsonResponse.getJSONArray("response");
-                JSONObject userInfo= jsonArray.getJSONObject(0);
-                firstName = userInfo.getString("first_name");
-                lastName = userInfo.getString("last_name");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            if(response != null && !response.equals("")) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jsonArray = jsonResponse.getJSONArray("response");
+                    String resultingString = "";
+                    for (int i=0; i< jsonArray.length(); i++){
 
-            String resultingString = "Имя: " + firstName + "\n" + "Фамилия: " + lastName;
-            result.setText(resultingString);
+                        JSONObject userInfo = jsonArray.getJSONObject(i);
+                        firstName = userInfo.getString("first_name");
+                        lastName = userInfo.getString("last_name");
+                      resultingString += "Имя: " + firstName + "\n" + "Фамилия: " + lastName + "\n\n" ;
+                    }
+                    result.setText(resultingString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            showResultText();
+        }
+            else{
+                showErrorText();
+            }
+            loading.setVisibility(View.INVISIBLE);
         }
     }
 
